dcc88f286d55c773bfb77c1780c98ab0be8a9a6a
althryenix <tameralkim@gmail.com>
2022-12-11 00:11:43 +0300

    dev

--- src/App.js
+++ src/App.js
@@ -12,9 +12,10 @@ function App() {
     window.location.href = "./";
   }
 
-  window.scrollTo(0,0);
+  window.scrollTo(0, 0);
 
   const StyledButton = (props) => (
+
     <Button
       disableRipple={true}
       disableTouchRipple={true}
@@ -42,96 +43,101 @@ function App() {
 
   return (
     <>
-    <div>
-      <div className="App">
-        <header className="App-header">
-          <h1 data-aos="fade-up" >tamer alkım tokuç</h1>
-          <h6 data-aos="fade-up" data-aos-delay="400">ted university 4<sup>th</sup> grade computer engineering student</h6>
-        </header>
+      <div>
+        <div className="App">
+          <header className="App-header" style={{ userSelect: "none" }}>
+            <h1 data-aos="fade-up" >tamer alkım tokuç</h1>
+            <h6 data-aos="fade-up" data-aos-delay="400">ted university 4<sup>th</sup> grade computer engineering student</h6>
+          </header>
+        </div>
+
+
+        <div className='Body-buttons' data-aos="fade-down" data-aos-delay="700" data-aos-duration="1000">
+
+          <StyledButton href="#personal">personal info</StyledButton>
+          <br />
+
+          <StyledButton href="#education">education</StyledButton>
+          <br />
+
+          <StyledButton href="#experience">experience</StyledButton>
+          <br />
+
+          <StyledButton href="#test">test</StyledButton>
+          <br />
+
+          <StyledButton href="#links">links</StyledButton>
+          <br />
+        </div>
+
+        <hr color="#404040" />
       </div>
 
+      <div>
+        <div className="section" id='personal'>
 
-      <div className='Body-buttons' data-aos="fade-down" data-aos-delay="700" data-aos-duration="1400">
+          <h3>Personal info</h3>
 
-        <StyledButton href="#personal">personal info</StyledButton>
-        <br />
+          <h4>who am i?</h4>
 
-        <StyledButton href="#education">education</StyledButton>
-        <br />
+          <p>I am Tamer Alkım Tokuç, a student from Turkey, Ankara. I like to design stuff, build stuff, and try to make them work.</p>
+          <p>I am playing drums since 2016. Art plays a big role in my life and I like it to reflect it on my work as well.</p>
+          <p>Motorsports are my passion and I volunteer as marshall in TOSFED/FIA.</p>
 
-        <StyledButton href="#experience">experience</StyledButton>
-        <br />
+          <h4>what do i do?</h4>
 
-        <StyledButton href="#test">test</StyledButton>
-        <br />
+          <p>
+            I can use Java to develop anything.
+            I started learning it in university and
+            improved myself by taking external courses and building projects.
+          </p>
+          <p>
+            I learned Python to improve my career 
+          </p>
+          <p>Frontend development</p>
 
-        <StyledButton href="#links">links</StyledButton>
-        <br />
-      </div>
+        </div>
 
-      <hr color="#404040" />
-    </div>
-      
+        <hr color="#404040" />
 
-      <div className="section" id='personal'>
+        <div className='section' id="education">
+          <h3>Education</h3>
+          <h4>school</h4>
+          <p>
+            currently studying in ted university, computer engineering department
+          </p>
+        </div>
 
-        <h3>Personal info</h3>
+        <hr color="#404040" />
 
-        <h4>who am i?</h4>
+        <div className='section' id="experience">
+          <h3>Experience</h3>
+          <h4>internship</h4>
+          <h5>infomedya ajans - ankara / turkey</h5>
 
-        <p>I am Tamer Alkım Tokuç, a student from Turkey, Ankara. I like to design stuff, build stuff, and try to make them work.</p>
-        <p>I am playing drums since 2016. Art plays a big role in my life and I like it to reflect it on my work as well.</p>
-        <p>Motorsports are my passion and I volunteer as marshall in TOSFED/FIA.</p>
+          <p>
+            lorem ipsum
+          </p>
+        </div>
 
-        <h4>what do i do?</h4>
+        <hr color="#404040" />
 
-        <p>
-          I can use Java to develop anything. I started learning it in university and improved myself by taking external courses and building projects.
-          I also took algorithms and data structures courses to improve my abilities.
-        </p>
-        <p>Frontend development</p>
+        <div className='section' id="test">
+          <h3>Test</h3>
+          <p>lorem</p>
+        </div>
 
-      </div>
+        <hr color="#404040" />
 
-      <hr color="#404040" />
+        <div className='section' id="links">
+          <h3>Links</h3>
+          <h4>-----github link------</h4>
+          <h4>-----linkedin link------</h4>
+          <h4>-----hackerrank link------</h4>
+          <h4>-----codegolf link------</h4>
+          <h4>-----stack link------</h4>
 
-      <div className='section' id="education">
-        <h3>Education</h3>
-        <h4>school</h4>
-        <p>
-          currently studying in ted university, computer engineering department
-        </p>
-      </div>
-
-      <hr color="#404040" />
-
-      <div className='section' id="experience">
-        <h3>Experience</h3>
-        <h4>internship</h4>
-        <h5>infomedya ajans - ankara / turkey</h5>
-
-        <p>
-          lorem ipsum
-        </p>
-      </div>
-
-      <hr color="#404040" />
-
-      <div className='section' id="test">
-        <h3>Test</h3>
-        <p>lorem</p>
-      </div>
-
-      <hr color="#404040" />
-
-      <div className='section' id="links">
-        <h3>Links</h3>
-        <h4>-----github link------</h4>
-        <h4>-----linkedin link------</h4>
-        <h4>-----hackerrank link------</h4>
-        <h4>-----codegolf link------</h4>
-        <h4>-----stack link------</h4>
-
+        </div>
       </div>
     </>
   );
